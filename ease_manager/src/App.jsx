b619a7a7d59a9f4b69255b6fe4be490a4ebea04d
Swathi-Maniraj <manirajmadhu1974@gmail.com>
2024-08-13 11:23:52 +0530

    routes

--- ease_manager/src/App.jsx
+++ ease_manager/src/App.jsx
@@ -2,13 +2,15 @@ import {BrowserRouter,Routes,Route} from "react-router-dom"
 import './App.css'
 import Dashboard from "./pages/Dashboard"
 import Login from "./pages/Login"
+import Home from "./pages/Home"
 function App() {
   return (
     <BrowserRouter>
     <Routes>
+      <Route path="/" element={<Home/>}/>
       <Route path="/Dashboard" element ={<Dashboard/>}/>  
       <Route path="/login" element ={<Login/>}/>  
-      <Route path="/room_allocation" element ={<SignIn/>}/>
+      {/* <Route path="/room_allocation" element ={<SignIn/>}/> */}
     </Routes>
     </BrowserRouter>
   )
