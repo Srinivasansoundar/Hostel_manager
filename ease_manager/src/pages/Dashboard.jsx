60d05520f344aeb64d556f9bee256526f2bdc6db
Steve B <121563726+Steve-B17@users.noreply.github.com>
2024-08-13 06:36:00 +0000

    Dashboard changes

--- ease_manager/src/pages/Dashboard.jsx
+++ ease_manager/src/pages/Dashboard.jsx
@@ -2,10 +2,24 @@ import React from 'react'
 
 const Dashboard = () => {
   return (
-   <>
-   <header>
-    <h1 className="text-lg text-red-200" > Hostel Ease Manager </h1>
-    </header></>
+    <>
+      <header>
+        <div className='block  w-1/5 h-svh border-2 '>
+          <div>Hello</div>
+        </div>
+        <div className="right w-3/5 h-svh ">
+          <div>
+            <div className="font-bold">
+              Hostel Ease Manager
+            </div>
+            <div className="div"></div>
+            <div className="div"></div>
+            <div className="div"></div>
+            <div className="div"></div>
+          </div>
+
+        </div>
+      </header></>
   )
 }
 
