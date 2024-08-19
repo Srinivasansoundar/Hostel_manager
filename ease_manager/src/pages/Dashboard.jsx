fb2d4fff91eeb46e55bd5df77ebcbb140668adc0
Srinivasansoundar <srinisoundar66@gmail.com>
2024-08-19 06:18:44 +0000

    first step

--- ease_manager/src/pages/Dashboard.jsx
+++ ease_manager/src/pages/Dashboard.jsx
@@ -2,24 +2,40 @@ import React from 'react'
 
 const Dashboard = () => {
   return (
-    <>
-      <header>
-        <div className='block  w-1/5 h-svh border-2 '>
-          <div>Hello</div>
-        </div>
-        <div className="right w-3/5 h-svh ">
-          <div>
-            <div className="font-bold">
-              Hostel Ease Manager
-            </div>
-            <div className="div"></div>
-            <div className="div"></div>
-            <div className="div"></div>
-            <div className="div"></div>
-          </div>
+    // <>
+    //   <header class="w-full">
+    //     <div className='block  w-1/5 h-svh border-2 '>
+    //       <div>Hello</div>
+    //     </div>
+    //     <div className="right w-3/5 h-svh ">
+    //       <div>
+    //         <div className="font-bold">
+    //           Hostel Ease Manager
+    //         </div>
+    //         <div className="div"></div>
+    //         <div className="div"></div>
+    //         <div className="div"></div>
+    //         <div className="div"></div>
+    //       </div>
 
-        </div>
-      </header></>
+    //     </div>
+    //   </header>
+    //   </>
+    <header className="flex">
+  <div className='block w-1/5 h-screen border-2'>
+    <div>Hello</div>
+  </div>
+  <div className="right w-4/5 h-screen flex flex-col">
+    <div className="font-bold">
+      Hostel Ease Manager
+    </div>
+    <div className="div"></div>
+    <div className="div"></div>
+    <div className="div"></div>
+    <div className="div"></div>
+  </div>
+</header>
+
   )
 }
 
